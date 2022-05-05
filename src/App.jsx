0ea6b6f8c7e84e7115cbec64fd03269b60842a51
Jacegk1999 <Jacegk1999@gmail.com>
2022-05-05 16:15:55 -0700

    started routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-export default function App() {
-  return <h1>Hello World</h1>;
-}
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import Home from './views/Home/Home';
+import Login from './views/Login/login';
+
+render(
+  <>
+    <Login />
+    <Home />
+  </>
+);
